af1a3a11c70fa25c85f53945393ea443c97977c2
Marcus Liebhardt <ich@marcusliebhardt.de>
2012-05-04 18:59:07 +0900

    copied simulation packages to new stack/repo

---
+++ kobuki_gazebo_plugins/build/CMakeFiles/gazebo_ros_kobuki.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gazebo_ros_kobuki.dir/src/gazebo_ros_kobuki.o"
+  "../lib/libgazebo_ros_kobuki.pdb"
+  "../lib/libgazebo_ros_kobuki.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gazebo_ros_kobuki.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ kobuki_gazebo_plugins/build/CMakeFiles/rospack_genmsg_all.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rospack_genmsg_all"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/rospack_genmsg_all.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
