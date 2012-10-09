22256da9c9c3d141d21ed183ef6d05808666f8c2
Marcus Liebhardt <ich@marcusliebhardt.de>
2012-10-09 10:18:20 +0900

    now it should remove build folders

--- kobuki_gazebo_plugins/build/CMakeFiles/rospack_genmsg_all.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/rospack_genmsg_all"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/rospack_genmsg_all.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
