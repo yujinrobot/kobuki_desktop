5f771b317016b118f5dcb3f554973c1bdb5043d9
Daniel Stonier <d.stonier@gmail.com>
2012-11-13 04:45:55 +0900

    code is ready to go for gyro drift.

--- kobuki_qtestsuite/src/kobuki_qtestsuite/gyro_drift_frame.py
+++ kobuki_qtestsuite/src/kobuki_qtestsuite/gyro_drift_frame.py
@@ -18,6 +18,7 @@ import roslib
 roslib.load_manifest('kobuki_qtestsuite')
 import rospy
 from qt_gui_py_common.worker_thread import WorkerThread
+from kobuki_testsuite import ScanToAngle, DriftEstimation
 
 # Local resource imports
 import detail.common_rc
@@ -32,8 +33,8 @@ class GyroDriftFrame(QFrame):
     def __init__(self, parent=None):
         super(GyroDriftFrame, self).__init__(parent)
         self._ui = Ui_gyro_drift_frame()
-        #self._motion = Rotate('/cmd_vel')
-        #self.robot_state_subscriber = rospy.Subscriber("/mobile_base/events/robot_state", RobotStateEvent, self.robot_state_callback)
+        self._motion = DriftEstimation('/odom','/scan_angle','/cmd_vel')
+        self._scan_to_angle = ScanToAngle('/scan','/scan_angle')
         self._motion_thread = None
 
     def setupUi(self):
@@ -44,7 +45,8 @@ class GyroDriftFrame(QFrame):
 
     def shutdown(self):
         rospy.loginfo("Kobuki TestSuite: gyro drift shutdown")
-        #self._motion.shutdown()
+        self._motion.shutdown()
+        self._scan_to_angle.shutdown()
 
     ##########################################################################
     # Motion Callbacks
@@ -61,15 +63,15 @@ class GyroDriftFrame(QFrame):
     def on_start_button_clicked(self):
         self._ui.start_button.setEnabled(False)
         self._ui.stop_button.setEnabled(True)
-        #self._motion_thread = WorkerThread(self._motion.execute, self._run_finished)
-        #self._motion_thread.start()
+        self._motion_thread = WorkerThread(self._motion.execute, self._run_finished)
+        self._motion_thread.start()
 
     @Slot()
     def on_stop_button_clicked(self):
         self.stop()
         
     def stop(self):
-        #self._motion.stop()
+        self._motion.stop()
         if self._motion_thread:
             self._motion_thread.wait()
         self._ui.start_button.setEnabled(True)
