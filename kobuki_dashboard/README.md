da40a9d42254aec2f6577f48643211fe3f21efd9
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-01-23 18:56:28 +0900

    adds renamed turtlebot2_dashboard (now kobuki_dashboard)

---
+++ kobuki_dashboard/README.md
@@ -0,0 +1,14 @@
+Kobuki Dashboard
+====================
+
+This is a dashboard for [Kobuki](http://kobuki.yujinrobot.com/).
+
+It is based on the [rqt_robot_dashboard](https://github.com/ros-visualization/rqt_robot_plugins) framework and runs in [rqt](https://github.com/ros-visualization/rqt).
+You can run this dashboard by calling:
+
+`rosrun kobuki_dashboard kobuki_dashboard`
+
+or through rqt:
+
+`rqt -s kobuki_dashboard`
+
---
+++ kobuki_dashboard/src/kobuki_dashboard/dashboard.py
@@ -0,0 +1,61 @@
+import roslib;roslib.load_manifest('kobuki_dashboard')
+import rospy
+
+import diagnostic_msgs
+from linux_hardware.msg import LaptopChargeStatus
+
+from rqt_robot_dashboard.dashboard import Dashboard
+from rqt_robot_dashboard.widgets import MonitorDashWidget, ConsoleDashWidget, MenuDashWidget, BatteryDashWidget, IconToolButton, NavViewDashWidget
+from QtGui import QMessageBox, QAction
+from python_qt_binding.QtCore import QSize
+
+from .led_widget import LedWidget
+from .motor_widget import MotorWidget
+
+class KobukiDashboard(Dashboard):
+    def setup(self, context):
+        self.message = None
+
+        self._dashboard_message = None
+        self._last_dashboard_message_time = 0.0
+
+        self._motor_widget = MotorWidget('/mobile_base/commands/motor_power')
+        self._laptop_bat = BatteryDashWidget("Laptop")
+        self._kobuki_bat = BatteryDashWidget("Kobuki")
+
+        self._dashboard_agg_sub = rospy.Subscriber('diagnostics_agg', diagnostic_msgs.msg.DiagnosticArray, self.dashboard_callback)
+        self._laptop_bat_sub = rospy.Subscriber('/laptop_charge', LaptopChargeStatus, self.laptop_cb)
+
+    def get_widgets(self):
+        leds = [LedWidget('/mobile_base/commands/led1'), LedWidget('/mobile_base/commands/led2')]
+
+        return [[MonitorDashWidget(self.context), ConsoleDashWidget(self.context), self._motor_widget], leds, [self._laptop_bat, self._kobuki_bat], [NavViewDashWidget(self.context)]]
+
+    def dashboard_callback(self, msg):
+        self._dashboard_message = msg
+        self._last_dashboard_message_time = rospy.get_time()
+
+        for status in msg.status:
+            if status.name == "/Kobuki/Motor State":
+                motor_state = int(status.values[0].value)
+                self._motor_widget.update_state(motor_state)
+
+            elif status.name == "/Power System/Battery":
+                for value in status.values:
+                    if value.key == 'Percent':
+                        self._kobuki_bat.update_perc(float(value.value))
+                        self._kobuki_bat.update_time(float(value.value))
+                    elif value.key == "State":
+                        if value.value == "Charging":
+                            self._kobuki_bat.set_charging(True)
+                        else:
+                            self._kobuki_bat.set_charging(False)
+
+    def laptop_cb(self, msg):
+        self._laptop_bat.update_perc(float(msg.percentage))
+        self._laptop_bat.update_time(float(msg.percentage))
+        self._laptop_bat.set_charging(bool(msg.charge_state))
+
+    def shutdown_dashboard(self):
+        self._laptop_bat_sub.unregister()
+        self._dashboard_agg_sub.unregister()
