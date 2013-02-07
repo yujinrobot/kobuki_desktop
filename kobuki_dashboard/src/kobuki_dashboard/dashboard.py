dc219966fe46243118162e5a55934bc15ce27ee0
Daniel Stonier <d.stonier@gmail.com>
2013-02-07 16:33:44 +0900

    remove need for linux hardware explicitly, use diagnostics parsing instead.

--- kobuki_dashboard/src/kobuki_dashboard/dashboard.py
+++ kobuki_dashboard/src/kobuki_dashboard/dashboard.py
@@ -2,7 +2,6 @@ import roslib;roslib.load_manifest('kobuki_dashboard')
 import rospy
 
 import diagnostic_msgs
-from linux_hardware.msg import LaptopChargeStatus
 
 from rqt_robot_dashboard.dashboard import Dashboard
 from rqt_robot_dashboard.widgets import MonitorDashWidget, ConsoleDashWidget, MenuDashWidget, BatteryDashWidget, IconToolButton
@@ -24,7 +23,6 @@ class KobukiDashboard(Dashboard):
         self._kobuki_bat = BatteryDashWidget("Kobuki")
 
         self._dashboard_agg_sub = rospy.Subscriber('diagnostics_agg', diagnostic_msgs.msg.DiagnosticArray, self.dashboard_callback)
-        self._laptop_bat_sub = rospy.Subscriber('/laptop_charge', LaptopChargeStatus, self.laptop_cb)
 
     def get_widgets(self):
         leds = [LedWidget('/mobile_base/commands/led1'), LedWidget('/mobile_base/commands/led2')]
@@ -33,7 +31,8 @@ class KobukiDashboard(Dashboard):
     def dashboard_callback(self, msg):
         self._dashboard_message = msg
         self._last_dashboard_message_time = rospy.get_time()
-
+        
+        laptop_battery_status = {}
         for status in msg.status:
             if status.name == "/Kobuki/Motor State":
                 motor_state = int(status.values[0].value)
@@ -43,18 +42,24 @@ class KobukiDashboard(Dashboard):
                 for value in status.values:
                     if value.key == 'Percent':
                         self._kobuki_bat.update_perc(float(value.value))
+                        # This should be self._last_dashboard_message_time?
+                        # Is it even used graphically by the widget
                         self._kobuki_bat.update_time(float(value.value))
                     elif value.key == "Charging State":
                         if value.value == "Trickle Charging" or value.value == "Full Charging":
                             self._kobuki_bat.set_charging(True)
                         else:
                             self._kobuki_bat.set_charging(False)
+            elif status.name == "/Power System/Laptop Battery":
+                for value in status.values:
+                    laptop_battery_status[value.key]=value.value
 
-    def laptop_cb(self, msg):
-        self._laptop_bat.update_perc(float(msg.percentage))
-        self._laptop_bat.update_time(float(msg.percentage))
-        self._laptop_bat.set_charging(bool(msg.charge_state))
+        if (laptop_battery_status):
+            percentage = float(laptop_battery_status['Charge (Ah)'])/float(laptop_battery_status['Capacity (Ah)'])
+            self._laptop_bat.update_perc(percentage*100)
+            self._laptop_bat.update_time(percentage*100)
+            charging_state = True if float(laptop_battery_status['Current (A)']) > 0.0 else False
+            self._laptop_bat.set_charging(charging_state)
 
     def shutdown_dashboard(self):
-        self._laptop_bat_sub.unregister()
         self._dashboard_agg_sub.unregister()
