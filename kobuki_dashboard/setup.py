95b69ee4e67043321e69fecf66758b15bb6a51cd
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-06-12 16:06:33 +0900

    kobuki_dashboard: adds missing files

---
+++ kobuki_dashboard/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(packages=['kobuki_dashboard'],
+                             scripts=['scripts/kobuki_dashboard'],
+                             package_dir={'': 'src'})
+
+setup(**d)
