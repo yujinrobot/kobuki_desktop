9a4a305384a20f5689c8d10d2e0663c5b34c080e
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-07-11 18:19:38 +0900

    adds rqt and more python related fixes

--- kobuki_qtestsuite/setup.py
+++ kobuki_qtestsuite/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(packages=['kobuki_qtestsuite'],
+d = generate_distutils_setup(packages=['kobuki_qtestsuite','kobuki_qtestsuite.detail'],
                              package_dir={'': 'src'})
 
 setup(**d)
